4ebc8d384c90dda71bd0dc9890d674c4a8e90401
Weiting-Xia <xwt20030711@gmail.com>
2026-01-22 23:20:06 -0500

    Update loading picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     locale: "en_US",
     images: [
       {
-        url: "https://cv.jarocki.me/opengraph-image",
+        url: "/og.png",
         width: 1200,
         height: 630,
         alt: `${RESUME_DATA.name}'s profile picture`,
@@ -36,7 +36,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: `${RESUME_DATA.name}`,
     description: RESUME_DATA.about,
-    images: ["https://cv.jarocki.me/opengraph-image"],
+    images: ["/og.png"],
   },
 };
 
